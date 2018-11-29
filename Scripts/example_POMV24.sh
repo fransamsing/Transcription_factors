452f062b886bbb39ba00836fcf52cc961bb8fcfd
Samsing Pedrals, Francisca (A&F, Hobart) <Francisca.Samsingpedrals@csiro.au>
2018-11-29 18:27:54 +1100

    function running

--- Scripts/example_POMV24.sh
+++ Scripts/example_POMV24.sh
@@ -3,5 +3,5 @@ python get_sequences.py \
 -filepath /home/sam079/RNAseq-POMV/Results/ControlvsPOMV24_ALL.csv \
 -gff /OSM/CBR/AF_POMV/work/POMV_RNA_seq/Genomes/Salmo_salar/GCF_000233375.1_ICSASG_v2_genomic.gff \
 -genome /OSM/CBR/AF_POMV/work/POMV_RNA_seq/Genomes/Salmo_salar//GCF_000233375.1_ICSASG_v2_genomic.fna \
+-background_out /home/sam079/Transcription_factors/Results/background_sequences_POMV24.txt \
 -target_out /home/sam079/Transcription_factors/Results/target_sequences_POMV24.txt \
--background_out /home/sam079/Transcription_factors/Results/background_sequences_POMV24.txt
